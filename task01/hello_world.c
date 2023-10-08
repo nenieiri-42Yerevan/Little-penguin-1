74e5ba913184d8fe8be51212e427ecfc2b0acbe1
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-08 13:55:34 +0400

    [Task01] fix bugs

--- task01/hello_world.c
+++ task01/hello_world.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:18:25 by vismaily          #+#    #+#             */
-/*   Updated: 2023/10/06 12:46:54 by vismaily         ###   ########.fr       */
+/*   Updated: 2023/10/08 13:37:33 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,21 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-static int __init
-hello_init(void)
+MODULE_AUTHOR("Volodya Ismailyan");
+MODULE_DESCRIPTION("This is a Hello_world module");
+MODULE_VERSION("1.0.0");
+MODULE_LICENSE("GPL");
+
+static int __init hello_init(void)
 {
-	printk(KERN_INFO "Hello world !\n");
+	printk(KERN_DEBUG "Hello world !\n");
 	return (0);
 }
 
-static void __exit
-hello_cleanup(void)
+static void __exit hello_cleanup(void)
 {
-	printk(KERN_INFO "Cleaning up module.\n");
+	printk(KERN_DEBUG "Cleaning up module.\n");
 }
 
 module_init(hello_init);
 module_exit(hello_cleanup);
-
-MOUDLE_AUTHOR("Volodya Ismailyan");
