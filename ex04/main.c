e911f6e010529327c8d24f0587ee25ad332c254f
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-14 20:58:19 +0400

    [ex04] update main.c

--- ex04/main.c
+++ ex04/main.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:18:25 by vismaily          #+#    #+#             */
-/*   Updated: 2023/10/14 19:10:05 by vismaily         ###   ########.fr       */
+/*   Updated: 2023/10/14 19:32:44 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 #include <linux/init.h>
 
 MODULE_AUTHOR("Volodya Ismailyan");
-MODULE_DESCRIPTION("This is my first driver");
+MODULE_DESCRIPTION("This is a keyboard driver");
 MODULE_VERSION("1.0.0");
 MODULE_LICENSE("GPL");
 
 static int __init init_driver(void)
 {
-	pr_info("Hello world ! I'm a driver for keyboard !\n");
+	pr_info("keyboard plugged in!\n");
 	return (0);
 }
 
 static void __exit cleanup_driver(void)
 {
-	pr_info("Cleaning up module driver.\n");
+	pr_info("Cleaning up module.\n");
 }
 
 module_init(init_driver);
