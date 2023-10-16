32d8ce29464f0316b8e8d3ad9e4148b49706fb0d
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-16 13:45:40 +0400

    [ex05] register major and minor numbers dynamically

--- ex05/main.c
+++ ex05/main.c
@@ -6,29 +6,40 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:18:25 by vismaily          #+#    #+#             */
-/*   Updated: 2023/10/08 18:42:59 by vismaily         ###   ########.fr       */
+/*   Updated: 2023/10/16 13:42:06 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
 
 MODULE_AUTHOR("Volodya Ismailyan");
-MODULE_DESCRIPTION("This is a Hello_world module");
+MODULE_DESCRIPTION("This is a misc char device driver");
 MODULE_VERSION("1.0.0");
 MODULE_LICENSE("GPL");
-MODULE_INFO(intree, "Y");
+
+dev_t	dev = 0;
 
 static int __init hello_init(void)
 {
-	printk(KERN_INFO "Hello world !\n");
+	if (alloc_chrdev_region(&dev, 0, 1, "fortytwo") < 0)
+	{
+		pr_info("Cannot allocate major number :(\n");
+		return (-1);
+	}
+	pr_info("Major number is %d and Minor number is %d\n",
+		MAJOR(dev), MINOR(dev));
+	pr_info("Fortytwo device driver inserted successfully :)\n");
 	return (0);
 }
 
 static void __exit hello_cleanup(void)
 {
-	printk(KERN_INFO "Cleaning up module.\n");
+	unregister_chrdev_region(dev, 1);
+	pr_info("Fortytwo device driver removed successfully :)\n");
 }
 
 module_init(hello_init);
