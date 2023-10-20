4b9b11389510e614565c8b22378516dff544bdcc
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-20 16:29:18 +0400

    [ex05] Implement the write function

--- ex05/main.c
+++ ex05/main.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:18:25 by vismaily          #+#    #+#             */
-/*   Updated: 2023/10/18 15:51:51 by vismaily         ###   ########.fr       */
+/*   Updated: 2023/10/20 16:28:34 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 
+# define USERNAME	"vismaily"
+# define USERNAME_LEN	8
+
 MODULE_AUTHOR("Volodya Ismailyan");
 MODULE_DESCRIPTION("This is a misc char device driver");
 MODULE_VERSION("1.0.0");
@@ -24,57 +27,48 @@ MODULE_LICENSE("GPL");
 
 static dev_t			dev = 0;
 static struct class		*dev_class;
-static struct cdev		fortytwo_cdev;
+static struct cdev		ft_cdev;
 static struct file_operations	fops;
 
 /*
- * This function will be called when we open the Device file
- */
-static int fortytwo_open(struct inode *_inode, struct file *_file)
-{
-	pr_info("1\n");
-	return (0);
-}
-
-/*
- * This function will be called when we close the Device file
- */
-static int fortytwo_release(struct inode *_inode, struct file *_file)
-{
-	pr_info("2\n");
-	return (0);
-}
-
-/*
- * This function will be called when we read the Device file
+ * This function will be called when we read from the Device
  */
 static ssize_t
-fortytwo_read(struct file *flip, char __user *buf, size_t len, loff_t *off)
+ft_read(struct file *f, char __user *buf, size_t len, loff_t *offset)
 {
 	pr_info("3\n");
 	return (0);
 }
 
 /*
- * This function will be called when we write the Device file
+ * This function will be called when we write to the Device
  */
 static ssize_t
-fortytwo_release(struct file *flip, const char __user *buf,
-		size_t len, loff_t *off)
+ft_write(struct file *f, const char __user *buf,
+		size_t len, loff_t *offset)
 {
-	pr_info("4\n");
-	return (0);
+	char	user_data[USERNAME_LEN + 1];
+
+	memset(user_data, 0, USERNAME_LEN + 1);
+	if (len != USERNAME_LEN && len != USERNAME_LEN + 1)
+		return (-EINVAL);
+	if (copy_from_user(user_data, buf, len))
+		return (-EIO);
+	if (len == (USERNAME_LEN + 1) && user_data[USERNAME_LEN] != '\0')
+		return (-EINVAL);
+	if (memcmp(user_data, USERNAME, USERNAME_LEN))
+		return (-EINVAL);
+	pr_info("line %d", __LINE__);
+	return (len);
 }
 
 static int __init hello_init(void)
 {
 	fops.owner = THIS_MODULE;
-	fops.open = fortytwo_open;
-	fops.read = fortytwo_read;
-	fops.write = fortytwo_write;
-	fops.release = fortytwo_release;
+	fops.read = ft_read;
+	fops.write = ft_write;
 
-	cdev_init(&fortytwo_cdev, &fops);
+	cdev_init(&ft_cdev, &fops);
 
 	/* Allocating Major number dynamically */
 	if (alloc_chrdev_region(&dev, 0, 1, "fortytwo") < 0) {
@@ -85,17 +79,17 @@ static int __init hello_init(void)
 		MAJOR(dev), MINOR(dev));
 
 	/* Adding character device to the system */
-	if (cdev_add(&fortytwo_cdev, dev, 1) < 0)
+	if (cdev_add(&ft_cdev, dev, 1) < 0)
 	{
 		pr_err("Cannot add the character device to the system\n");
 		goto r_class;
 	}
 
 	/* Creating struct class */
-	dev_class = class_create("fortytwo_class");
+	dev_class = class_create("ft_class");
 	if (IS_ERR(dev_class)) {
 		pr_err("Cannot create the struct class for device\n");
-		goto r_class;
+		goto r_cdev;
 	}
 
 	/* Creating device */
@@ -108,6 +102,8 @@ static int __init hello_init(void)
 
 r_device:
 	class_destroy(dev_class);
+r_cdev:
+	cdev_del(&ft_cdev);
 r_class:
 	unregister_chrdev_region(dev, 1);
 	return (-1);
@@ -117,7 +113,7 @@ static void __exit hello_cleanup(void)
 {
 	device_destroy(dev_class, dev);
 	class_destroy(dev_class);
-	cdev_del(&fortytwo_cdev);
+	cdev_del(&ft_cdev);
 	unregister_chrdev_region(dev, 1);
 	pr_info("Fortytwo device driver removed successfully :)\n");
 }
