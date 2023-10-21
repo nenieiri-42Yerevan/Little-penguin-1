8fc23893df867daca656647a9d999ebbe7a4efc9
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-21 17:38:08 +0400

    [ex05] Change project from char device to misc device

--- ex05/misc_device_driver.c
+++ ex05/misc_device_driver.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   misc_device_driver.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:18:25 by vismaily          #+#    #+#             */
-/*   Updated: 2023/10/20 18:12:04 by vismaily         ###   ########.fr       */
+/*   Updated: 2023/10/21 16:44:03 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/device.h>
-#include <linux/cdev.h>
+#include <linux/miscdevice.h>
 
 # define USERNAME	"vismaily"
 # define USERNAME_LEN	8
@@ -25,9 +25,7 @@ MODULE_DESCRIPTION("This is a misc char device driver");
 MODULE_VERSION("1.0.0");
 MODULE_LICENSE("GPL");
 
-static dev_t			dev = 0;
-static struct class		*dev_class;
-static struct cdev		ft_cdev;
+static struct miscdevice	misc_device;
 static struct file_operations	fops;
 
 /*
@@ -78,54 +76,24 @@ static int __init ft_driver_init(void)
 	fops.read = ft_read;
 	fops.write = ft_write;
 
-	cdev_init(&ft_cdev, &fops);
+	misc_device.minor = MISC_DYNAMIC_MINOR;
+	misc_device.name = "fortytwo";
+	misc_device.fops = &fops;
 
-	/* Allocating Major number dynamically */
-	if (alloc_chrdev_region(&dev, 0, 1, "fortytwo") < 0) {
-		pr_err("Cannot allocate major number :(\n");
+	if (misc_register(&misc_device))
+	{
+		pr_err("misc_register failed!!! :(\n");
 		return (-1);
 	}
-	pr_info("Major number is %d and Minor number is %d\n",
-		MAJOR(dev), MINOR(dev));
 
-	/* Adding character device to the system */
-	if (cdev_add(&ft_cdev, dev, 1) < 0)
-	{
-		pr_err("Cannot add the character device to the system\n");
-		goto r_class;
-	}
-
-	/* Creating struct class */
-	dev_class = class_create("ft_class");
-	if (IS_ERR(dev_class)) {
-		pr_err("Cannot create the struct class for device\n");
-		goto r_cdev;
-	}
-
-	/* Creating device */
-	if (IS_ERR(device_create(dev_class, NULL, dev, NULL, "fortytwo"))) {
-		pr_err("Cannot create the Device\n");
-		goto r_device;
-	}
-	pr_info("Fortytwo device driver inserted successfully :)\n");
+	pr_info("Fortytwo misc device driver inserted successfully :)\n");
 	return (0);
-
-r_device:
-	class_destroy(dev_class);
-r_cdev:
-	cdev_del(&ft_cdev);
-r_class:
-	unregister_chrdev_region(dev, 1);
-	return (-1);
 }
 
 static void __exit ft_driver_exit(void)
 {
-	device_destroy(dev_class, dev);
-	class_destroy(dev_class);
-	cdev_del(&ft_cdev);
-	unregister_chrdev_region(dev, 1);
-	pr_info("Fortytwo device driver removed successfully :)\n");
+	misc_deregister(&misc_device);
+	pr_info("Fortytwo misc device driver removed successfully :)\n");
 }
 
 module_init(ft_driver_init);
