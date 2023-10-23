68de1f043cbae520d2fe6171d75f75ce53c47861
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-23 20:28:55 +0400

    [ex07] Create files apropriate commands in fortytwo tree

--- ex07/main.c
+++ ex07/main.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:18:25 by vismaily          #+#    #+#             */
-/*   Updated: 2023/10/22 22:46:58 by vismaily         ###   ########.fr       */
+/*   Updated: 2023/10/23 20:25:08 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,48 @@ MODULE_VERSION("1.0.0");
 MODULE_LICENSE("GPL");
 MODULE_INFO(intree, "Y");
 
-static struct dentry	*ft_dentry;
+static struct dentry	*db_fortytwo;
+static struct dentry	*db_id;
+static struct dentry	*db_jiffies;
+static struct dentry	*db_foo;
 
 static int __init debugfs_init(void)
 {
-	ft_dentry = debugfs_create_dir("fortytwo", NULL);
-	if (!ft_dentry)
+	db_fortytwo = debugfs_create_dir("fortytwo", NULL);
+	if (!db_fortytwo)
 	{
 		pr_err("Failed to create 'fortytwo' directory in debugfs");
 		return (-1);
 	}
+
+	db_id = debugfs_create_file("id", 0666, db_fortytwo, NULL, NULL);
+	if (!db_fortytwo)
+		goto clean_fortytwo;
+
+	db_jiffies = debugfs_create_file("jiffies", 0444, db_fortytwo,
+			NULL, NULL);
+	if (!db_jiffies)
+		goto clean_id;
+
+	db_foo = debugfs_create_file("foo", 0644, db_fortytwo, NULL, NULL);
+	if (!db_foo)
+		goto clean_jiffies;
+
 	printk(KERN_INFO "Inserting debugfs module!\n");
 	return (0);
+
+clean_jiffies:
+	debugfs_remove(db_jiffies);
+clean_id:
+	debugfs_remove(db_id);
+clean_fortytwo:
+	debugfs_remove(db_fortytwo);
+	return (-1);
 }
 
 static void __exit debugfs_cleanup(void)
 {
-	debugfs_remove_recursive(ft_dentry);
+	debugfs_remove_recursive(db_fortytwo);
 	printk(KERN_INFO "Cleaning up debugfs module.\n");
 }
 
