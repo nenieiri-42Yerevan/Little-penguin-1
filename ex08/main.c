fea2929df589d803b4368918298d1ff2f413621b
Volodya Ismailyan <nenie_iri@mail.ru>
2023-10-30 13:25:46 +0400

    [ex08] Correct the file

---
+++ ex08/main.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/29 12:43:54 by vismaily          #+#    #+#             */
+/*   Updated: 2023/10/30 13:15:54 by vismaily         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
+MODULE_DESCRIPTION("Useless module");
+
+static ssize_t	myfd_read(struct file *fp, char __user *user,
+				size_t size, loff_t *offs);
+static ssize_t	myfd_write(struct file *fp, const char __user *user,
+				size_t size, loff_t *offs);
+
+static struct file_operations	myfd_fops = {
+	.owner	= THIS_MODULE,
+	.read	= &myfd_read,
+	.write	= &myfd_write
+};
+
+static struct miscdevice	myfd_device = {
+	.minor	= MISC_DYNAMIC_MINOR,
+	.name	= "reverse",
+	.fops	= &myfd_fops
+};
+
+static char	str[PAGE_SIZE];
+
+/*
+ * 0x0 = '\0'
+ */
+ssize_t	myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
+{
+	size_t	i;
+	size_t	j;
+	int	status;
+	char	*tmp;
+
+	if (strlen(str) == 0)
+		return (0);
+	tmp = kmalloc(sizeof(char) * (strlen(str) + 1), GFP_KERNEL);
+	if (!tmp)
+		return (-ENOMEM);
+	for (i = 0, j = strlen(str) - 1; j != 0; --j, ++i)
+		tmp[i] = str[j];
+	tmp[i++] = str[j--];
+	tmp[i] = 0x0;
+	status = simple_read_from_buffer(user, size, offs, tmp, i);
+	kfree(tmp);
+	return (status);
+}
+
+ssize_t	myfd_write(struct file *fp, const char __user *user, size_t size,
+			loff_t *offs)
+{
+	ssize_t	res;
+
+	res = simple_write_to_buffer(str, PAGE_SIZE - 1, offs, user, size);
+	if (res >= 0)
+		str[res++] = 0x0;
+	return (res);
+}
+
+static int __init myfd_init(void)
+{
+	int retval;
+
+	memset(str, 0, PAGE_SIZE);
+	retval = misc_register(&myfd_device);
+	return (retval);
+}
+
+static void __exit myfd_cleanup(void)
+{
+	misc_deregister(&myfd_device);
+}
+
+module_init(myfd_init);
+module_exit(myfd_cleanup);
